58645519923b99af3d68d49ba77b9a5b563b249f
svartmouse <svartmouse@hotmail.com>
2015-06-01 23:25:38 +0100

    problems
    will be fixed

---
+++ run.bat
@@ -0,0 +1,14 @@
+@echo off
+rem The location of Gource. Set this to the root directory where gource.exe is.
+set gourceloc=C:\Users\Shawn cockburn\Downloads\gource-0.42.win32
+
+rem The git location. Set this to the git /bin folder.
+set gitloc=C:\Users\Shawn cockburn\Downloads\Git\bin
+
+rem Set the path.
+set path=%path%;%gourceloc%;%gitloc%
+
+rem Run gource.
+
+rem Pause.
+pause
