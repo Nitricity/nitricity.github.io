1c12e929e39d86b15dbb036230ccd40b9850ec86
svartmouse <10scockburn@A22-11.BHS.local>
2015-06-03 16:18:41 +0100

    may have to delete this

--- gitstatus.bat
+++ gitstatus.bat
@@ -5,12 +5,13 @@ set path=%path%;E:\tedlar\Git\bin
 
 set /p driveltr=Enter drive letter (without colon): 
 %driveltr%:
+set /p dirname=Press Enter once: 
 cd /nitricity\nitricity.github.io
 
-git add *
-set /p commitmsg=Enter commit message: 
-git commit -m "%commitmsg%"
+git status
 
-git push
-
-PAUSE
+:command
+set /p cmd=Enter command:
+%cmd%
+goto command
+pause
