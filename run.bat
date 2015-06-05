94e9dcdc82c4657bff9d3ddcfa7904f7f19bd14e
svartmouse <svartmouse@hotmail.com>
2015-06-05 10:08:41 +0100

    run

--- run.bat
+++ run.bat
@@ -9,7 +9,7 @@ rem Set the path.
 set path=%path%;%gourceloc%;%gitloc%
 
 rem Run gource.
-gource --user-image-dir H:\avatar -f -1280x720 -e 1.0 --bloom-multiplier 2.0 --bloom-intensity 1.0 --key --title "Nitricity" --logo H:\avatar\nitricity.jpg --seconds-per-day 20 --windowed
+gource --user-image-dir H:\avatar -f -1280x720 -e 1.0 --bloom-multiplier 2.0 --bloom-intensity 1.0 --key --title "Nitricity" --logo H:\avatar\nitricity.jpg --seconds-per-day 10 --windowed
 
 rem Pause.
 pause
