ddc06f7ef6c274369bcefb1bf9f3c594b466163d
svartmouse <svartmouse@hotmail.com>
2015-11-04 23:26:10 -0500

    reorganise
    ...

--- gource.bat
+++ gource.bat
@@ -1,2 +1,2 @@
-C:\Users\shawn\Downloads\gource-0.42.win32\gource -1280x720 --user-image-dir C:\Users\shawn\Downloads\gource-0.42.win32\avatars -a 1 -s 5 -e 0.1
+C:\Users\shawn\Downloads\gource-0.42.win32\gource -1280x720 --user-image-dir C:\Users\shawn\Downloads\gource-0.42.win32\avatars --key -a 1 -s 5 -e 0.1
 pause
